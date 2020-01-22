22ae9955d6ddc35d10f97c8d6818ec65fb1623bb
Ultra03 <20672260+Ultra03@users.noreply.github.com>
2020-01-21 21:48:36 -0800

    dsniff sha256

--- Formula/dsniff.rb
+++ Formula/dsniff.rb
@@ -14,7 +14,7 @@ end
 class Dsniff < Formula
   homepage 'http://monkey.org/~dugsong/dsniff/'
   url 'http://monkey.org/~dugsong/dsniff/beta/dsniff-2.4b1.tar.gz'
-  sha1 '25cfea26e9bbe016451180758a49d4d5bc9317d3'
+  sha256 'a9803a7a02ddfe5fb9704ce86f0ffc48453c321e88db85810db411ba0841152a'
 
   depends_on NoBdb5
   depends_on 'libnet'
